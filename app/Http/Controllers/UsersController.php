da305e0fe800afe1d8f989e6866d9f49e4a6d9e5
WangSail <624440896@qq.com>
2018-08-08 17:20:16 +0000

    Only authorized user can edit user profile

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -8,7 +8,13 @@ use App\Http\Requests\UserRequest;
 use App\Handlers\ImageUploadHandler;
 
 class UsersController extends Controller
-{
+{   
+
+    public function __construct()
+    {
+        $this->middleware('auth', ['except' => ['show']]);
+    }
+
     //Route::get('/users/{user}', 'UsersController@show')->name('users.show');
     // 变量名匹配路由片段 User 对应 {user}
     public function show(User $user)
@@ -18,14 +24,15 @@ class UsersController extends Controller
     
     //更新个人资料页面显示
     public function edit(User $user)
-    {
+    {   $this->authorize('update', $user);
     	return view('users.edit',compact('user'));
     }
 
     //提交个人资料更新修改
     public function update(UserRequest $request, ImageUploadHandler $uploader, User $user){
+        
+        $this->authorize('update', $user);
         $data = $request->all();
-
         if ($request->avatar) {
             $result = $uploader->save($request->avatar, 'avatars', $user->id,362);
             if ($result) {
