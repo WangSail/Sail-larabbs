6bcb00c0a52c885cfa679fb8ff7bf3026cdce752
WangSail <624440896@qq.com>
2018-08-10 16:09:15 +0000

    Topic detail page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,8 +8,8 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>@yield('title', 'LaraBBS') - Laravel 进阶教程</title>
-
+    <title>@yield('title', 'LaraBBS') - Laravel 进阶教程</title>    
+    <meta name="description" content="@yield('description', 'LaraBBS 爱好者社区')" />
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     @yield('styles')
