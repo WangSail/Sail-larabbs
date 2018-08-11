70cba077679c7458e3a2a6016d95321e011e66f1
WangSail <624440896@qq.com>
2018-08-11 07:22:20 +0000

    Auto translate slug

--- config/services.php
+++ config/services.php
@@ -34,5 +34,10 @@ return [
         'key' => env('STRIPE_KEY'),
         'secret' => env('STRIPE_SECRET'),
     ],
+    
+    'baidu_translate' => [
+        'appid' => env('BAIDU_TRANSLATE_APPID'),
+        'key'   => env('BAIDU_TRANSLATE_KEY'),
+    ],
 
 ];
